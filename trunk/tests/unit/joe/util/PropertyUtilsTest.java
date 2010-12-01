0bc3c1a579f8373b587820e7134ae8ef79cb9b5f
joe.j.kearney <joe.j.kearney@656360dd-6331-1122-41c8-6b68e2546ef7>
2010-12-01 16:04:40 +0000

    Restructuring, build changes

---
+++ trunk/tests/unit/joe/util/PropertyUtilsTest.java
@@ -0,0 +1,32 @@
+package joe.util;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableMap;
+
+public class PropertyUtilsTest {
+	@Test
+	public void testPropertyResolveTrivial() throws Exception {
+		Function<String, String> propertyResolverFromMap = PropertyUtils.propertyResolverFromMap(ImmutableMap.<String, String> of());
+		assertThat(propertyResolverFromMap.apply("abc"), is("abc"));
+	}
+	@Test
+	public void testPropertyResolveSingle() throws Exception {
+		Function<String, String> propertyResolverFromMap = PropertyUtils.propertyResolverFromMap(ImmutableMap.of("k", "v"));
+		assertThat(propertyResolverFromMap.apply("abc${k}def"), is("abcvdef"));
+	}
+	@Test
+	public void testPropertyResolveIndirect() throws Exception {
+		Function<String, String> propertyResolverFromMap = PropertyUtils.propertyResolverFromMap(ImmutableMap.of("key1", "${key2}", "key2", "v"));
+		assertThat(propertyResolverFromMap.apply("abc${key1}def"), is("abcvdef"));
+	}
+	@Test
+	public void testPropertyResolveTwo() throws Exception {
+		Function<String, String> propertyResolverFromMap = PropertyUtils.propertyResolverFromMap(ImmutableMap.of("key1", "v1", "key2", "v2"));
+		assertThat(propertyResolverFromMap.apply("abc${key1}de${key2}f"), is("abcv1dev2f"));
+	}
+}
