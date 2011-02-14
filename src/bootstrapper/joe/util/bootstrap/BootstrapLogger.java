79e7ebd5e15ae42f7323fbc97f17d25d6cdce698
Joe Kearney <joekearney@users.noreply.github.com>
2011-02-14 18:45:34 +0000

    minor botstrapper changed, and r05 jar checkins

--- src/bootstrapper/joe/util/bootstrap/BootstrapLogger.java
+++ src/bootstrapper/joe/util/bootstrap/BootstrapLogger.java
@@ -1,6 +1,6 @@
 package joe.util.bootstrap;
 
-import static com.google.common.collect.Lists.*;
+import static com.google.common.collect.Lists.newLinkedList;
 
 import java.util.Date;
 import java.util.Map;
@@ -15,11 +15,16 @@ final class BootstrapLogger {
 		this.applicationProperties = applicationProperties;
 	}
 
-	void log(String string) {
+	/**
+	 * Logs the message directly or stashes in a queue until {@link #flushLogQueue()} is called.
+	 * 
+	 * @param message log message
+	 */
+	void log(String message) {
 		if (!isLoggingDisabled()) {
 			final String logMessage = String.format("%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL [%2$s] - %3$s",
-					new Date(), Thread.currentThread().getName(), string);
-	
+					new Date(), Thread.currentThread().getName(), message);
+
 			if (queueLogMessages && isLoggingEnabled()) {
 				// we're in queue mode and must now flush everything
 				logQueue.add(logMessage);
@@ -34,6 +39,10 @@ final class BootstrapLogger {
 		}
 	}
 
+	/**
+	 * Writes out all queued log messages if logging has been explicitly enabled, else clears the queue, permanently
+	 * discarding the messages.
+	 */
 	void flushLogQueue() {
 		if (isLoggingEnabled()) {
 			queueLogMessages = false;
@@ -45,11 +54,23 @@ final class BootstrapLogger {
 		}
 	}
 
+	/**
+	 * Has logging been explicitly disabled?
+	 * 
+	 * @return {@code true} iff the property {@link BootstrapMain#BOOTSTRAP_ENABLE_LOGGING_KEY} has been set to
+	 *         {@code false}
+	 */
 	boolean isLoggingDisabled() {
-		return "false".equalsIgnoreCase(applicationProperties.get(BootstrapMain.BOOTSTRAP_PROPERTY_LOGGING_KEY));
+		return "false".equalsIgnoreCase(applicationProperties.get(BootstrapMain.BOOTSTRAP_ENABLE_LOGGING_KEY));
 	}
 
-	private boolean isLoggingEnabled() {
-		return "true".equalsIgnoreCase(applicationProperties.get(BootstrapMain.BOOTSTRAP_PROPERTY_LOGGING_KEY));
+	/**
+	 * Has logging been explicitly enabled?
+	 * 
+	 * @return {@code true} iff the property {@link BootstrapMain#BOOTSTRAP_ENABLE_LOGGING_KEY} has been set to
+	 *         {@code true}
+	 */
+	boolean isLoggingEnabled() {
+		return "true".equalsIgnoreCase(applicationProperties.get(BootstrapMain.BOOTSTRAP_ENABLE_LOGGING_KEY));
 	}
 }
