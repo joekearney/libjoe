f32fb1db22abc466e855a9980832bb067606d47d
Joe Kearney <joekearney@users.noreply.github.com>
2011-02-15 18:23:47 +0000

    fix string un-escaping to do what I expected it to do

---
+++ tests/unit/joe/util/StringUnescapeTest.java
@@ -0,0 +1,24 @@
+package joe.util;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class StringUnescapeTest {
+	private static String windowsNewline = "\r\f";
+	private static String unixNewline = "\n";
+	
+	@Test
+	public void testUnescapeWindowsNewline() throws Exception {
+		String unescaped = StringUtils.unescapeJava(windowsNewline);
+		assertThat(unescaped.length(), is(4));
+		assertThat(unescaped, is("\\r\\f"));
+	}
+	@Test
+	public void testUnescapeUnixNewline() throws Exception {
+		String unescaped = StringUtils.unescapeJava(unixNewline);
+		assertThat(unescaped.length(), is(2));
+		assertThat(unescaped, is("\\n"));
+	}
+}
